41998d9a434bfaedb351ef8a352b59ccacc649c3
YBJF <778349156@qq.com>
2020-02-11 17:26:28 +0800

    Add files via upload

---
+++ stock.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mate-terminal -t "123" -x bash -c "echo "0324" |sudo -S chmod 777 /home/yczheng/python_program/get_stock_price/1_4pi.py;python /home/yczheng/python_program/get_stock_price/1_4pi.py;exec bash;"
+#echo "0324" |sudo -S chmod 777 /home/yczheng/python_program/get_stock_price/1_4pi.py
+#python /home/yczheng/python_program/get_stock_price/1_4pi.py
